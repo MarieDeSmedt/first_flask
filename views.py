b6dee3d9eefc9665384ddcb827e87cbd649f81b2
mari <desmedtmarie@outlook.com>
2022-04-11 11:43:28 +0200

    first commit

---
+++ views.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+def hello(name):
+    return "hello " +name
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
