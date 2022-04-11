8dcfbffb61cffbcf422d8f99fac6dec2365f654e
mari <desmedtmarie@outlook.com>
2022-04-11 15:13:35 +0200

    html

--- fbapp/views.py
+++ fbapp/views.py
@@ -17,6 +17,10 @@ def hello(name):
 def index():
     return render_template('index.html')
 
+@app.route('/result/')
+def result():
+    return render_template('result.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
