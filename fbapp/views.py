f6cb992fa2d4d1282520ca75899801994b54623d
mari <desmedtmarie@outlook.com>
2022-04-11 15:35:45 +0200

    html suite

--- fbapp/views.py
+++ fbapp/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 
@@ -15,11 +15,25 @@ def hello(name):
 @app.route("/")
 @app.route("/index/")
 def index():
-    return render_template('index.html')
+    description = "Connecte-toi à Facebook afin de connaître les résultats !"
+    return render_template('index.html',
+                       
+                         user_name = "TOM",
+                        user_image = url_for('static',filename='tmp/cover_111823112767411.jpg'),
+                        description = description,
+                        blur=True)
 
 @app.route('/result/')
 def result():
-    return render_template('result.html')
+    description= """
+      Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton émission préférée ! Bientôt tu partiras les cheveux au vent sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle chance !
+    """
+    return render_template('result.html',
+                        user_name = "TOM",
+                        user_image = url_for('static',filename='tmp/cover_111823112767411.jpg'),
+                        description = description,
+                        blur = True
+                        )
 
 
 if __name__ == "__main__":
